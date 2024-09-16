a038c1659e0f97d3842d1a9c8e5585c7bd5b1e86
Marcellnagy05 <marcika863@gmail.com>
2024-09-16 12:46:05 +0200

    Add project files.

---
+++ FociWebapp13BC/Models/FociDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FociWebapp13BC.Models
+{
+    public class FociDbContext : DbContext
+    {
+        public FociDbContext(DbContextOptions<FociDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Meccs> Meccsek { get; set; }
+    }
+}
---
+++ FociWebapp13BC/Models/Meccs.cs
@@ -0,0 +1,14 @@
+﻿namespace FociWebapp13BC.Models
+{
+    public class Meccs
+    {
+        public int ID { get; set; }
+        public int Fordulo { get; set; }
+        public int HazaiVeg { get; set; }
+        public int VendegVeg { get; set; }
+        public int HazaiFelido { get; set; }
+        public int VendegFelido { get; set; }
+        public string HazaiCsapat { get; set; }
+        public string VendegCsapat { get; set; }
+    }
+}
